d7848fc202b2c52ec54a690803bf90bf579dbe60
arsantin <contato@andesantin.com.br>
2020-07-18 19:46:31 -0300

    fetching cafes

--- pages/cafes-coloniais/index.js
+++ pages/cafes-coloniais/index.js
@@ -1,7 +1,32 @@
-function CafesColoniais(){
-  return  <div>
-  <h1>CAFÉS COLONIAIS</h1></div>
-  ;
+import Link from 'next/link';
+
+// cafes will be populated at build time by getStaticProps()
+function CafesColoniais({ cafes }) {
+  return (
+    <ul>
+      {cafes.map((cafe) => (
+        <li><Link href="/"><a>{cafe.nome}</a></Link></li>
+      ))}
+    </ul>
+  )
+}
+
+// This function gets called at build time on server-side.
+// It won't be called on client-side, so you can even do
+// direct database queries. See the "Technical details" section.
+export async function getStaticProps() {
+  // Call an external API endpoint to get cafes.
+  // You can use any data fetching library
+  const res = await fetch('https://curitibacolonial.com.br/apicomercios/')
+  const cafes = await res.json()
+
+  // By returning { props: cafes }, the CafesColoniais component
+  // will receive `cafes` as a prop at build time
+  return {
+    props: {
+      cafes,
+    },
+  }
 }
 
 export default CafesColoniais
