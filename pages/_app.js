c828bb24581a1cb69950c96530b4bb71bcbddfdd
santin <contato@brutalweb.com.br>
2020-08-09 21:13:46 -0300

    adicionado nprogress

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,22 @@
 import App from 'next/app'
 import Head from 'next/head'
 import React from 'react'
+import Router from 'next/router'
+import NProgress from 'nprogress'
 import "antd/dist/antd.css";
 
+Router.events.on("routeChangeStart", (url)=>{
+  console.log(`Loading ${url}`);
+  NProgress.start();
+});
+
+Router.events.on("routeChangeComplete", (url)=>{
+  NProgress.done();
+});
+
+Router.events.on("routeChangeError", (url)=>{
+  NProgress.done();
+});
 
 export default class MyApp extends App {
  
@@ -14,6 +28,9 @@ export default class MyApp extends App {
         <Head>
           <title>My new cool app</title>
           <link rel="stylesheet" href="./css/styles.css"></link>
+          <link rel="stylesheet" href="https://unpkg.com/nprogress@0.2.0/nprogress.css"></link>
+
+          
           <link href="https://fonts.googleapis.com/css2?family=Catamaran:wght@100;400;800;900&display=swap" rel="stylesheet"/>
           
         </Head>
